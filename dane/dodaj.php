51b102684e3a442c10388c832d922f9c0caef6b3
KlaudiaDługi <73891807+wolnaodnazwy@users.noreply.github.com>
2020-11-30 13:38:27 +0100

    Add files via upload

---
+++ dane/dodaj.php
@@ -0,0 +1,17 @@
+  <?php
+      $conn = mysqli_connect("localhost", "root", "", "dane")
+      or die ("Brak połączenia z bazą danych");
+
+      if (isset($_POST['submit'])) {
+        $title = $_POST['title'];
+        $type = $_POST['type'];
+        $year = $_POST['year'];
+        $rate = $_POST['rate'];
+
+        $zapytanie= "INSERT INTO 'filmy' ('gatunki_id', 'tytul', 'rok', 'ocena') VALUES ($type, $title, $year, $rate)";
+        $wynik = mysqli_query($conn, $zapytanie) or die ("Nie udało sie dodać do bazy danych.");
+        echo "Film: ".$title." został dodany do bazy.";
+      }
+
+      mysqli_close($conn);
+    ?>
