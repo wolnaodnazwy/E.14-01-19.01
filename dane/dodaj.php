c1adf653303f89fce52228ea4b2b81455a7fad5e
KlaudiaDługi <73891807+wolnaodnazwy@users.noreply.github.com>
2020-11-30 14:33:21 +0100

    Update dodaj.php

--- dane/dodaj.php
+++ dane/dodaj.php
@@ -8,7 +8,7 @@
         $year = $_POST['year'];
         $rate = $_POST['rate'];
 
-        $zapytanie= "INSERT INTO 'filmy' ('gatunki_id', 'tytul', 'rok', 'ocena') VALUES ($type, $title, $year, $rate)";
+        $zapytanie= "INSERT INTO filmy (gatunki_id, tytul, rok, ocena) VALUES ($type,'".$title."', $year, $rate)";
         $wynik = mysqli_query($conn, $zapytanie) or die ("Nie udało sie dodać do bazy danych.");
         echo "Film: ".$title." został dodany do bazy.";
       }
